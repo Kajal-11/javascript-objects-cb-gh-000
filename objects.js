afdb39c2de6fa2f7ec1219db9e3b748012236db0
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 11:20:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,12 @@ var playlist = {Kajal: "songA",
                 Sneh: "songB",
                 Harsh: "songC"};
 
-  
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
